48050697abadcac7f376f2697547f311c51554e3
Ondisk90 <ondisk9090@gmail.com>
2024-01-04 17:53:54 +0200

    Add project files.

---
+++ SharpFtpServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace SharpFtpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseDNS();
+            Console.WriteLine("User name exsist in user.xml");
+
+            Console.WriteLine("Start FTP Server");
+
+            //using (FtpServer server = new FtpServer(IPAddress.IPv6Any, 21))
+            using (FtpServer server = new FtpServer(IPAddress.Any, 21))
+            {
+                server.Start();
+
+                Console.WriteLine("Press any key to stop...");
+                Console.ReadKey(true);
+            }
+        }
+        static void UseDNS()
+        {
+            string hostName = Dns.GetHostName();
+            Console.WriteLine("Host Name = " + hostName);
+            IPHostEntry local = Dns.GetHostByName(hostName);
+            foreach (IPAddress ipaddress in local.AddressList)
+            {
+                Console.WriteLine("IPAddress = " + ipaddress.ToString());
+            }
+        }
+    }
+
+}
---
+++ SharpFtpServer/README.md
@@ -0,0 +1,18 @@
+# Sharp Ftp Server
+
+This software has been for internal using for transfer files with ip54 ftp
+It is a console application using Dot net version 4.8.
+
+The original was taking from
+Source: http://www.codeproject.com/Articles/380769/Creating-an-FTP-Server-in-Csharp-with-IPv-Support
+
+Author: Rick Bassham
+
+## Features
+* emable transfer files between 2 computer
+* enable add users and password.
+
+
+## Made with
+* Net Framework 4.8, C# 
+* Visual Studio 2022 
